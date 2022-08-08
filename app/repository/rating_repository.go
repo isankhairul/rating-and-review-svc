59e039aa942c42dc8efde4dea2e0755e17c5e5da
liman@klikdokter.com <liman@klikdokter.com>
2022-08-08 12:12:32 +0700

    fixing create submission

--- app/repository/rating_repository.go
+++ app/repository/rating_repository.go
@@ -265,7 +265,7 @@ func (r *ratingRepo) CreateRatingSubmission(input []request.SaveRatingSubmission
 			{"value", args.Value},
 			{"ip_address", args.IPAddress},
 			{"user_agent", args.UserAgent},
-			{"source_trans_id", args.SourceTransID},
+			{"source_trans_id", args.SourceTransID + "||" + args.RatingID},
 			{"user_platform", args.UserPlatform},
 			{"like_counter", 0},
 			{"created_at", time.Now().In(util.Loc)},
